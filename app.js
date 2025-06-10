659843ef5abf5c44c40fd9129cd285a0cda96e2d
nitesh <niteshkohli015@gmail.com>
2025-06-10 16:04:01 +0530

    router folder added and fixed how routes handled

--- app.js
+++ app.js
@@ -1,24 +1,15 @@
-//jshint esversion:6
+import express from 'express';
+import bodyParser from 'body-parser';
+import ejs from 'ejs';
+import _ from 'lodash';
+import Post from './models/post.js';
+import homeRouter from './routes/home.js';
+import aboutRouter from './routes/about.js';
+import contactRouter from './routes/contact.js';
+import publishRouter from './routes/publish.js';
+import composeRouter from './routes/compose.js';
+import searchPostsRouter from './routes/searchPosts.js';
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
-const _ = require("lodash");
-const mongoose = require("mongoose");
-
-
-mongoose.connect("mongodb+srv://niteshkoli:test123@cluster0.dsilpre.mongodb.net/blogPostDB", {useNewUrlParser: true});
-
-const postSchema = {
-  title: String,
-  content: String
-};
-
-const Post = mongoose.model("Post",postSchema);
-
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
 
@@ -27,73 +18,34 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-let posts = [];
+//routes
+app.use('/',homeRouter);
+app.use('/about',aboutRouter);
+app.use('/contact',contactRouter);
+app.use('/publish',publishRouter);
+app.use('/compose',composeRouter);
+// app.use('/searchPosts',searchPostsRouter);
 
 
-
-app.get("/", function(req, res){
-  Post.find({})
-    .then(function(posts){
-      res.render("home", {
-        startingContent: homeStartingContent,
-        posts: posts
-        });
-    })
-    .catch(function(err){
-      console.log(err);
-    });
-});
-
-app.get("/about", function(req, res){
-  res.render("about", {aboutContent: aboutContent});
-});
-
-app.get("/contact", function(req, res){
-  res.render("contact", {contactContent: contactContent});
-});
-
-app.get("/compose", function(req, res){
-  res.render("compose");
-});
-
-
-app.post("/compose", async (req, res) => {
-  const post = new Post({
-    title: req.body.postTitle,
-    content: req.body.postBody
-  });
-
-  try{
-    await post.save();
-    res.redirect("/");
-  }
-  catch (err) {
-    console.error("Error saving post:", err);
-    res.status(500).send("Something went wrong.");
-  }
-});
-
-
-
-
-app.get("/posts/:postId", function(req, res){
+app.get('/posts/:postId', async (req, res) => {
   const requestedPostId = req.params.postId;
-  const requestedTitle = _.lowerCase(req.params.postName);
 
-  
+  try {
+    const post = await Post.findOne({ _id: requestedPostId });
 
-    Post.findOne({_id:requestedPostId})
-          .then(function(post){
-            res.render("post", {
-              title: post.title,
-              content: post.content
-            });
-          })
-          .catch(function(err){
-            console.log(err);
-          });
-  });
-
+    if (post) {
+      res.render('post', {
+        title: post.title,
+        content: post.content
+      });
+    } else {
+      res.status(404).send('Post not found');
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
+});
 
 app.listen(3000, function() {
   console.log("Server started at: http://localhost:3000");
---
+++ routes/contact.js
@@ -0,0 +1,11 @@
+import Router  from 'express';
+const router = Router();
+
+
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+router.get("/", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+export default router;
